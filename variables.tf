7bba97556426982efafdaa97c9c633fc8808707b
Michael Shavlyuk <ineyqq@gmail.com>
2020-06-11 13:25:58 +0300

    Remove default values for private and nointernet subnets

--- variables.tf
+++ variables.tf
@@ -22,11 +22,9 @@ variable vpc_public_subnet_cidrs {
 }
 variable vpc_private_subnet_cidrs {
   type = list(string)
-  default = []
 }
 variable vpc_nointernet_subnet_cidrs {
   type = list(string)
-  default = []
 }
 
 #
